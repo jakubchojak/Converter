0c8ae472355108ed10bfef92e338bd3c105fd59f
Jakub Chojak <abukkajohc@gmail.com>
2022-10-19 11:16:18 +0200

    Initial Commit

---
+++ NumbersExchange/NumbersExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersExchangeApp.swift
+//  NumbersExchange
+//
+//  Created by Jakub Chojak on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NumbersExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
